e117cca949eca6854b4ec575aec3b0f1d9937e50
Gustavo-MedeirosS <gustavo.msilva@sptech.school>
2025-03-05 15:00:14 -0300

    feat: adding quiz with navigation and results

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+        val etName: EditText = findViewById(R.id.etName)
+
+        btnStart.setOnClickListener {
+            if (etName.text.isEmpty()) {
+                Toast.makeText(
+                    this, "Please, enter your name", Toast.LENGTH_LONG
+                ).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
